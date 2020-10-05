fda035a3947f1e76cb48760a4f0b4ecca4799570
abrarulhaque <71403629+abrarulhaque@users.noreply.github.com>
2020-10-05 17:57:23 +0600

    Create pivalues.py

---
+++ pivalues.py
@@ -0,0 +1,15 @@
+i=1
+j=0
+x=0
+while True:
+    y=(-1)**j
+    p=1/i
+    q=y*p
+    x=x+q
+    j=j+1
+    i=i+2
+    if j%10000==0:
+        q=input("press 'q' to quit:")
+        if q=='q':
+            break
+print(4*x)
